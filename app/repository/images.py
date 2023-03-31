08e82fbd6add01a92897b3eada27069b303d9254
TT1410 <112493080+TT1410@users.noreply.github.com>
2023-03-31 23:00:05 +0300

    logout user and update some code
    Додано апі метод для виходу юзера (acces_token додається у blacklist на час його дії).
    
    Також внесено незначні зміни до коду

--- app/repository/images.py
+++ app/repository/images.py
@@ -6,11 +6,11 @@ async def create_image(user_id: int, description: str, uuid: str, db: AsyncSessi
     """
     The create_image function creates a new image in the database.
 
-    :param current_user: int: Get the current user
-    :param description: str: Get description for image
-    :param uuid: str: Get hash for image
-    :param db: AsyncSession: Pass in the database session to the function
-    :return: A user object
+    :param user_id: int: Identify the user who uploaded the image
+    :param description: str: Set the description of the image
+    :param uuid: str: Identify the image
+    :param db: AsyncSession: Pass in the database session to be used
+    :return: An image object
     """
     image = Image(
         user_id=user_id,
--- app/services/cloudinary.py
+++ app/services/cloudinary.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import BinaryIO
+from typing import BinaryIO, Optional
 
 import cloudinary
 from cloudinary.api import resource
@@ -15,7 +15,7 @@ cloudinary.config(
 )
 
 
-def upload_image(file: BinaryIO) -> str:
+def upload_image(file: BinaryIO) -> Optional[str]:
     """
     The upload_image function takes a file object and uploads it to Cloudinary.
     It returns the public ID of the uploaded image.
@@ -25,12 +25,26 @@ def upload_image(file: BinaryIO) -> str:
     """
     file_id = settings.cloudinary_folder + uuid.uuid4().hex
 
-    upload(file, public_id=file_id, owerwrite=True)
+    try:
+        upload(file, public_id=file_id, owerwrite=True)
+    except cloudinary.exceptions.Error as e:
+        return
 
     return file_id
 
 
 def get_format_image(file_id: str, width: int = 250, height: int = 250, crop: str = 'fill'):
+    """
+    The get_format_image function takes a file_id, width, height and crop as parameters.
+    The function returns the url of an image with the given dimensions and cropping mode.
+
+
+    :param file_id: str: Identify the image in cloudinary
+    :param width: int: Set the width of the image
+    :param height: int: Set the height of the image
+    :param crop: str: Crop the image
+    :return: The url of the image with the specified width, height and crop
+    """
     return cloudinary.CloudinaryImage(file_id).build_url(
         width=width,
         height=height,
