4fd53a09544183c7668313f0c924e78408b6299c
Valeri <uxenos3@gmail.com>
2023-04-01 08:42:10 +0300

    Update images.py
    Змінено пост рутер.

--- app/routes/images.py
+++ app/routes/images.py
@@ -18,6 +18,28 @@ from app.services.auth import auth_service
 router = APIRouter(prefix="/images", tags=["images"])
 
 
+@router.post("/", response_model=ImageCreateResponse, dependencies=[Depends(RateLimiter(times=10, seconds=60))])
+async def upload_image(file: UploadFile = File(), description: str = Form(min_length=10, max_length=1200), db: AsyncSession = Depends(get_db),
+                        current_user: User = Depends(auth_service.get_current_user)):
+    """
+    The upload_image function uploads the image to Cloudinary.
+
+    :param file: UploadFile: Get the file that is uploaded
+    :param db: AsyncSession: Get the database session
+    :param current_user: User: Get the current user
+    :return: The uploaded image
+    """
+    loop = asyncio.get_event_loop()
+    
+    try:
+        file_id = await loop.run_in_executor(None, cloudinary.upload_image, file.file)
+    
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid image file")
+
+    return await repository_images.create_image(current_user.id, description, file_id, db)
+
+
 @router.get("/", response_model=ImageGetResponse, dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def dowload_image(file_id: str, current_user: User = Depends(auth_service.get_current_user)):
     """
