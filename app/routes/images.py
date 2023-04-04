97b4c8c09306eac8af2621767ffe56912e501dd5
AndrewCheUA <107930151+AndrewCheUA@users.noreply.github.com>
2023-04-04 23:37:28 +0300

    Update images.py

--- app/routes/images.py
+++ app/routes/images.py
@@ -12,16 +12,19 @@ from app.schemas.image import (
     ImagePublic,
 )
 from app.services import cloudinary
-from app.services.auth import auth_service
+from app.services.auth import AuthService
 
 
-router = APIRouter(prefix="/images", tags=["images"])
+router = APIRouter(prefix="/images", tags=["Images"])
 
 
-@router.post("/", response_model=ImageCreateResponse, dependencies=[Depends(RateLimiter(times=10, seconds=60))])
+@router.post(
+    "/", response_model=ImageCreateResponse, response_model_by_alias=False, status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(RateLimiter(times=10, seconds=60))]
+)
 async def upload_image(file: UploadFile = File(), description: str = Form(min_length=10, max_length=1200),
                        db: AsyncSession = Depends(get_db),
-                       current_user: User = Depends(auth_service.get_current_user)):
+                       current_user: User = Depends(AuthService.get_current_user)):
     """
     The upload_image function is used to upload an image to the cloudinary server.
     The function takes in a file, description and database session as parameters.
@@ -35,49 +38,51 @@ async def upload_image(file: UploadFile = File(), description: str = Form(min_le
     :return: A dict with the following keys:
     """
     loop = asyncio.get_event_loop()
-    file_id = await loop.run_in_executor(None, cloudinary.upload_image, file.file)
+    image = await loop.run_in_executor(None, cloudinary.upload_image, file.file)
 
-    if file_id is None:
+    if image is None:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid image file")
 
-    image = await repository_images.create_image(current_user.id, description, file_id, db)
+    image = await repository_images.create_image(current_user.id, description.strip(), image['public_id'], db)
 
-    return {"image": image, "detail": "Image successfully created"}
+    return {"image": image, "detail": "Image successfully uploaded"}
 
 
-@router.get("/", response_model=ImageGetResponse, dependencies=[Depends(RateLimiter(times=10, seconds=60))])
-async def dowload_image(file_id: str, current_user: User = Depends(auth_service.get_current_user)):
+@router.get("/{image_id}", response_model=ImagePublic, dependencies=[Depends(RateLimiter(times=10, seconds=10))])
+async def get_image(image_id: int, current_user: User = Depends(AuthService.get_current_user),
+                    db: AsyncSession = Depends(get_db)):
     """
-    The dowload_image function dowloads the image from Cloudinary.
+    The get_image function returns an image by its id.
 
-    :param file_id: str: Get the file_url to download
-    :return: image url
+    :param image_id: int: Get the image id from the url
+    :param current_user: User: Get the current user from the database
+    :param db: AsyncSession: Pass the database session to the function
+    :return: The image object
     """
-    if current_user:
-        loop = asyncio.get_event_loop()
-        url = await loop.run_in_executor(None, cloudinary.get_format_image, file_id)
-    if url is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url does not exist")
+    image = await repository_images.get_image_by_id(current_user.id, image_id, db)
+    if image is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found image")
 
-    return url
+    return image
 
 
 @router.put("/description", response_model=ImagePublic, dependencies=[Depends(RateLimiter(times=10, seconds=60))])
-async def update_description(uuid: str, description: str = Form(min_length=10, max_length=1200), db: AsyncSession = Depends(get_db),
-                       current_user: User = Depends(auth_service.get_current_user)):
+async def update_description(image_id: int, description: str = Form(min_length=10, max_length=1200),
+                             db: AsyncSession = Depends(get_db),
+                             current_user: User = Depends(AuthService.get_current_user)):
     """
     The update_description function updates the description of an image.
         The function takes in the description to be updated.
         It also takes in a database session and current_user (the user who is making this request).
 
-    :param uuid: str: Get the unique link from the request      
+    :param image_id: int: Get the unique link from the request      
     :param description: Form: Get the description from the request
     :param db: AsyncSession: Get the database session
     :param current_user: User: Get the current user from the database
     :return: An image object
     """
-    updated_image = await repository_images.update_description(current_user.id, uuid, description, db)
-        
+    updated_image = await repository_images.update_description(current_user.id, image_id, description, db)
+
     if updated_image is None:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid identifier")
 
