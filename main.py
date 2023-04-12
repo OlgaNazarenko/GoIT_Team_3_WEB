57d898d521f73eb9cb0bdf7e169ea210a2798106
AndrewCheUA <andreicheban@gmail.com>
2023-04-12 17:02:58 +0300

    updated

--- main.py
+++ main.py
@@ -116,4 +116,4 @@ app.include_router(router, prefix=API_PREFIX)
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
